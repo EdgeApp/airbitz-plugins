bad89dd607f721f9e17ffe7e800ffe9ea2d2da79
Tim Horton <tim@airbitz.co>
2015-03-12 14:40:31 -0700

    Added redirect for unregistered user.

--- plugins/glidera/app/core/history.js
+++ plugins/glidera/app/core/history.js
@@ -1,25 +1,37 @@
+(function() {
+  'use strict';
 
-angular.module('app.history', []).
-run(function($rootScope, $window) {
-  var history = [];
-  $rootScope.$on('$locationChangeStart', function(event, next, current) {
-    console.log('history: ' + next + ' ' + current);
-    if (history.length == 0) {
-      history.push(current);
-    }
-    // if we are on the dashboard screen, empty history
-    if (next.match(/\.html#\/exchange\/$/)
-      || next.match(/\.html#\/signup\/$/)) {
-      history.length = 0;
-    }
-    history.push(next);
-  });
-  Airbitz._bridge.back = function() {
-    var el = history.pop();
-    if (history.length == 0) {
-      Airbitz._bridge.exit();
-    } else {
-      $window.history.back();
-    }
+  angular
+    .module('app.history', ['app.dataFactory'])
+    .run(['$rootScope', '$window', '$location', 'UserFactory', history]);
+
+  /* TODO: remove match(//) regexp and replace with state lookups */
+  function history($rootScope, $window, $location, UserFactory) {
+    var history = [];
+    $rootScope.$on('$locationChangeStart', function(event, next, current) {
+      if (!UserFactory.isRegistered() && !next.match(/signup.html/)) {
+        console.log('Not registered. Redirecting to registration form.');
+        $location.path('/signup/');
+        return;
+      }
+      console.log('history: ' + next + ' ' + current);
+      if (history.length == 0) {
+        history.push(current);
+      }
+      // if we are on the dashboard screen, empty history
+      if (next.match(/\.html#\/exchange\/$/)
+        || next.match(/\.html#\/signup\/$/)) {
+        history.length = 0;
+      }
+      history.push(next);
+    });
+    Airbitz._bridge.back = function() {
+      var el = history.pop();
+      if (history.length == 0) {
+        Airbitz._bridge.exit();
+      } else {
+        $window.history.back();
+      }
+    };
   };
-});
+})();
