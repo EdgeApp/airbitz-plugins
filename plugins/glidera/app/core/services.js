303e940a131da9868ba4e0231d1d6f1b025367f5
Damian <i3inary@gmail.com>
2015-03-12 19:53:20 -0700

    Removed error factory after testing new module

--- plugins/glidera/app/core/services.js
+++ plugins/glidera/app/core/services.js
@@ -111,38 +111,6 @@ factory('UserFactory', [
     return factory;
 
   }]).
-
-factory('Error', ['$state',
-  function($state) {
-  var factory = {
-    'reject': reject
-  };
-  return factory;
-
-  function reject(res) {
-    if (!res.code) {
-      Airbitz.ui.showAlert('Error', 'An unknown error occurred.');
-    } else {
-      if (res.code == 'MissingRequiredParameter') {
-        Airbitz.ui.showAlert('Error', 'The request is missing required data.');
-      } else if (res.code == 'InvalidParameterValue') {
-        Airbitz.ui.showAlert('Invalid', res.message);
-      } else if (res.code == 'UnauthorizedException') {
-        Airbitz.ui.showAlert('Unauthorized', res.message);
-      } else if (res.code == 'ConflictException') {
-        if (res.message && res.message.match(/email not verified/i)) {
-          $state.go('verifyEmail');
-          Airbitz.ui.showAlert('Error', 'Please verify your email address before continuing.');
-        } else {
-          Airbitz.ui.showAlert('Error', res.message);
-        }
-      } else {
-        Airbitz.ui.showAlert('Default Error', "An unknown error occurred.");
-      }
-    }
-  }
-}]).
-
 factory('DataFactory', [
   '$q', '$filter', 'States', 'ExchangeFactory', 'glideraFactory', 'TwoFactor', 'Prices',
   function($q, $filter, States, ExchangeFactory, glideraFactory, TwoFactor, Prices) {
