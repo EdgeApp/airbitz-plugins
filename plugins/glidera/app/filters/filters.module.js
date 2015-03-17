0efe411db04503fbc2a40e5046f6f929e82da156
Tim Horton <tim@airbitz.co>
2015-03-16 22:04:14 -0700

    Refactored filters module

---
+++ plugins/glidera/app/filters/filters.module.js
@@ -0,0 +1,32 @@
+(function() {
+  angular.module('app.filters', [])
+    .filter('statusFilter', statusFilter)
+    .filter('titlecase', titleCase);
+
+  function statusFilter() {
+    return function(status) {
+      return (status) ? 'Verified' : 'Unverified';
+    };
+  }
+
+  function titleCase() {
+    return function (input) {
+      var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
+
+      return input.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title){
+        if (index > 0 && index + match.length !== title.length &&
+          match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
+          (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
+          title.charAt(index - 1).search(/[^\s-]/) < 0) {
+          return match.toLowerCase();
+        }
+
+        if (match.substr(1).search(/[A-Z]|\../) > -1) {
+          return match;
+        }
+
+        return match.charAt(0).toUpperCase() + match.substr(1);
+      });
+    }
+  }
+})();
