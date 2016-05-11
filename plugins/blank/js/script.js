906840a7b5d4fadd1cb0400de9058bf72fb25ba4
Tim Horton <tim@airbitz.co>
2016-05-11 14:41:23 -0700

    Added a sample plugin

---
+++ plugins/blank/js/script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var qrcode = null;
+
+function updateUi(wallet) {
+  $('#walletName').text(wallet.name);
+  Airbitz.core.createReceiveRequest(wallet, {
+    label: "Blank App Request",
+    category: "Income:Plugin",
+    notes: "Income generated from a plugin",
+    amountSatoshi: 0,
+    amountFiat: 0,
+    success: function(data) {
+      var address = data["address"];
+      $('#address').text(address);
+      qrcode.clear();
+      qrcode.makeCode('bitcoin:' + address);
+    },
+    error: function() {
+      $('#address').text('');
+      Airbitz.ui.showAlert("Wallet Error", "Unable to load request!");
+    }
+  });
+}
+
+$(function() {
+  Airbitz.ui.title('Blank Plugin');
+  qrcode = new QRCode(document.getElementById("qrcode"), {
+    text: '',
+    width: 128,
+    height: 128,
+  });
+  // If the user changes the wallet, we want to know about it
+  Airbitz.core.setWalletChangeListener(function(wallet) {
+    Airbitz.ui.showAlert("Wallet Changed", "Wallet Changed to " + wallet.name + ".");
+    updateUi(wallet);
+  });
+  // After loading, lets fetch the currently selected wallet
+  Airbitz.core.selectedWallet({
+      success: updateUi,
+      error: function() {
+          Airbitz.ui.showAlert("Wallet Error", "Unable to load wallet!");
+      }
+  });
+});
