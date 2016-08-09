e9d903fa5be2b8aed48547c40a760a4e4a328b2c
Jonatan Lundin <jonatan.lundin@gmail.com>
2016-08-09 15:45:49 +0200

    Add initial widget build

---
+++ plugins/bitrefill/js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var selectedWallet = null;
+
+Airbitz.ui.title('Bitrefill');
+
+function onWalletChange (wallet) {
+  selectedWallet = wallet;
+}
+
+// If the user changes the wallet, we want to know about it
+Airbitz.core.setupWalletChangeListener(onWalletChange);
+
+// After loading, lets fetch the currently selected wallet
+Airbitz.core.getSelectedWallet({
+    success: onWalletChange,
+    error: function() {
+      Airbitz.ui.showAlert("Wallet Error", "Unable to load wallet!");
+    }
+});
+
+BitRefillWidget('#widget', {
+  key: '1ysCmV4dCk9PXZvSBW7kVM',
+  baseUrl: 'https://draper-staging-api.bitrefill.com/widget',
+  showBTCAddress: false,
+  paymentButtons: [{
+    title: 'Pay With Account Balance',
+    callback: function (order) {
+      Airbitz.core.createSpendRequest(selectedWallet, order.payment.address, order.satoshiPrice, {
+        label: "BitRefill",
+        category: "",
+        notes: order.itemDesc,
+        success: function(response) {
+          if (response.back) {
+            console.log("User pressed back button. Funds not sent")
+          } else {
+            console.log("Bitcoin sent")
+          }
+        },
+        error: function() {
+          Airbitz.ui.showAlert("Payment Error", "Error sending funds");
+        }
+      });
+    }
+  }],
+  userEmail: 'test@testing.com',
+  sendBitrefillEmails: true
+});
